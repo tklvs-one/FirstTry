40ae4f4043085562df21a19573e6f1d6d5fdb6a2
lev <l_slonc@mail.ru>
2024-10-22 23:17:54 +1000

    second commit main

--- obj/Debug/net8.0/ForGreatGit.AssemblyInfo.cs
+++ obj/Debug/net8.0/ForGreatGit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ForGreatGit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cc856e53bd0e8f5e1dce94000571cd457b91405")]
 [assembly: System.Reflection.AssemblyProductAttribute("ForGreatGit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ForGreatGit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
